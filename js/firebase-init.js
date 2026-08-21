14e43d4e2154401d0599196f38f8ffb04b1c28b0
Alexander Dennhoven <alexhuertgen5413@gmail.com>
2026-08-21 17:25:46 +0200

    Add files via upload

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,5 +1,24 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
-import { getFirestore, doc, getDoc, setDoc, collection, addDoc, getDocs, query, orderBy, serverTimestamp, deleteDoc } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
+import { getFirestore, doc, getDoc, setDoc, collection, addDoc, getDocs, query, where, orderBy, serverTimestamp, deleteDoc, updateDoc } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
+import { 
+    getAuth, 
+    signInWithEmailAndPassword, 
+    createUserWithEmailAndPassword,
+    signInWithPopup, 
+    GoogleAuthProvider, 
+    GithubAuthProvider,
+    signOut, 
+    onAuthStateChanged,
+    sendEmailVerification,
+    sendPasswordResetEmail,
+    sendSignInLinkToEmail,
+    isSignInWithEmailLink,
+    signInWithEmailLink,
+    verifyBeforeUpdateEmail,
+    updatePassword,
+    linkWithPopup,
+    unlink
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBjFo-_ESLBveBnAy1Iq-dG2OOVWgyLBlA",
@@ -12,5 +31,41 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
 
-export { db, doc, getDoc, setDoc, collection, addDoc, getDocs, query, orderBy, serverTimestamp, deleteDoc };
+export { 
+    db, 
+    auth, 
+    googleProvider, 
+    githubProvider,
+    GoogleAuthProvider,
+    GithubAuthProvider,
+    doc, 
+    getDoc, 
+    setDoc, 
+    collection, 
+    addDoc, 
+    getDocs, 
+    query, 
+    where,
+    orderBy, 
+    serverTimestamp, 
+    deleteDoc,
+    updateDoc,
+    signInWithEmailAndPassword, 
+    createUserWithEmailAndPassword,
+    signInWithPopup,
+    signOut, 
+    onAuthStateChanged,
+    sendEmailVerification,
+    sendPasswordResetEmail,
+    sendSignInLinkToEmail,
+    isSignInWithEmailLink,
+    signInWithEmailLink,
+    verifyBeforeUpdateEmail,
+    updatePassword,
+    linkWithPopup,
+    unlink
+};
